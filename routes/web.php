2414009d9a9b2f69e4dd8efae0b099dc2ecdfb4d
caindecoder <caindecoder@users.noreply.github.com>
2024-12-10 10:05:00 +0530

    V2.0 Restructured Vue Pages

--- routes/web.php
+++ routes/web.php
@@ -21,16 +21,5 @@ Route::get('/subscriptions', [SubscriptionController::class, 'index'])->name('su
 Route::get('/subscriptions/create', [SubscriptionController::class, 'create'])->name('subscriptions.create');
 Route::post('/subscriptions', [SubscriptionController::class, 'store'])->name('subscriptions.store');
 
-//Vue
-Route::get('/{pathMatch}', function () {
-    return view('welcome');
-})->where('pathMatch', '.*');
+//Vue Routes
 
-Route::get('/api/websites', [WebsiteController::class, 'index']);
-Route::post('/api/websites', [WebsiteController::class, 'store']);
-
-Route::get('/api/subscriptions', [SubscriptionController::class, 'index']);
-Route::post('/api/subscriptions', [SubscriptionController::class, 'store']);
-
-Route::get('/api/posts', [PostController::class, 'index']);
-Route::post('/api/posts', [PostController::class, 'store']);
