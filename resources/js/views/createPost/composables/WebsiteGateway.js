62f6f5ad0421305599dbec8c66b3a24c65f4c9cb
caindecoder <caindecoder@users.noreply.github.com>
2024-12-18 15:18:37 +0530

    V3.5 Modular Functional Frontend enhanced

--- resources/js/views/createPost/composables/WebsiteGateway.js
+++ resources/js/views/createPost/composables/WebsiteGateway.js
@@ -1,7 +1,10 @@
+import Website from "../composables/Post.js";
+
 export default class WebsiteGateway {
     async fetchWebsites() {
         const response = await fetch('/api/websites');
         if (!response.ok) throw new Error('Failed to fetch websites');
-        return response.json();
+        const websites = await response.json();
+        return websites.map(data => new Website(data));
     }
 }
